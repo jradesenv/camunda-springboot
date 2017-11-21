17a6c5bcec3bd4931d8660b6a3fadc1d7521b5cc
Jean Robert <jean.alves@ntconsult.com.br>
2017-11-21 13:07:23 -0200

    project with error

---
+++ src/main/java/br/com/ntconsult/camundaProject/app/CamundaSpringbootApplication.java
@@ -0,0 +1,11 @@
+package br.com.ntconsult.camundaProject.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaSpringbootApplication {
+	public static void main(String... args) {
+		SpringApplication.run(CamundaSpringbootApplication.class, args);
+	}
+}
